e2a083034043123b3b86f8d673d702b314f85a7c
xRetry <marcoschwarz.at@gmail.com>
2022-12-26 13:40:09 +0100

    Finish verification service

--- src/verification_service.rs
+++ src/verification_service.rs
@@ -1,5 +1,6 @@
 extern crate reqwest; // 0.9.18
 
+use std::net::UdpSocket;
 use std::{thread, time::Duration};
 use std::io::Read;
 
@@ -10,15 +11,20 @@ fn check_connection() -> Result<String, reqwest::Error> {
     res.read_to_string(&mut body).unwrap();
 
     let vec: Vec<&str> = body.split("\n").collect();
-
     return Ok(vec[0].to_string());
 }
 
-fn broadcast(message: String) {
-    println!("Broadcasting: {}", message);
+fn broadcast(socket: &UdpSocket, message: String) {
+    socket.send_to(message.as_bytes(), "10.0.0.255:2010")
+        .expect("Unable to send data");
 }
 
 fn main() {
+    let socket = UdpSocket::bind("10.0.0.255:2010")
+        .expect("Unable to bind to broadcast address");
+    socket.set_broadcast(true)
+        .expect("Unable to set broadcast mode");
+    
     loop {
         let conn_result = check_connection();
         let line = match conn_result {
@@ -26,7 +32,7 @@ fn main() {
             Err(_) => continue
         };
 
-        broadcast(line);
+        broadcast(&socket, line);
         thread::sleep(Duration::from_secs(10))
     }
 }
